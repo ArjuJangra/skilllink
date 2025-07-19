cd0ad3e43113816d55f544edd0ad6ed55c070182
ArjuJangra <arjujangra0011@gmail.com>
2025-07-19 21:43:13 +0530

    Add ProviderLogged component and update router configuration

--- src/router/index.js
+++ src/router/index.js
@@ -1,37 +1,4 @@
-// import { createRouter, createWebHistory } from 'vue-router';
-// import LoginPage from '@/components/LoginPage.vue';
-// import SignupPage from '@/components/SignupPage.vue';
-// import HomePage from '@/components/HomePage.vue';
-// import UserDashboard from '@/components/UserDashboard.vue'; 
-// import BookingPage from '@/components/BookingPage.vue';
-// import HomeBoard from '@/components/HomeBoard.vue';
-// import HomeLogged from '@/components/HomeLogged.vue';
-// import AboutPage from '@/components/AboutPage.vue';
-// import ContactPage from '@/components/ContactPage.vue';
-// const routes = [
-//   { path: '/', redirect: '/homeboard' },
-//   { path: '/login', component: LoginPage },
-//   { path: '/signup', component: SignupPage },
-//     { path: '/home', component: HomePage }, 
-//     { path: '/dashboard', component: UserDashboard },
-//     { path: '/booking', component: BookingPage },
-//     {path: '/homeboard', component: HomeBoard},
-//     {path: '/homelogged', component: HomeLogged},
-//     {path: '/about', component: AboutPage},
-//     {path: '/contact', component: ContactPage},
-//    {
-//   path: '/profile',
-//   name: 'UserProfile',
-//   component: () => import('@/components/UserDashboard.vue')
-// }
-// ];
 
-// const router = createRouter({
-//   history: createWebHistory(),
-//   routes,
-// });
-
-// export default router;
 import { createRouter, createWebHistory } from 'vue-router';
 import LoginPage from '@/components/LoginPage.vue';
 import SignupPage from '@/components/SignupPage.vue';
@@ -43,6 +10,7 @@ import HomeLogged from '@/components/HomeLogged.vue';
 import AboutPage from '@/components/AboutPage.vue';
 import ContactPage from '@/components/ContactPage.vue';
 import HelpPage from '@/components/HelpPage.vue';
+import ProviderLogged from '@/components/ProviderLogged.vue';
 const routes = [
   { path: '/', redirect: '/homeboard' },
   { path: '/login', component: LoginPage },
@@ -55,7 +23,7 @@ const routes = [
   { path: '/about', component: AboutPage },
   { path: '/contact', component: ContactPage },
   {path: '/help', component: HelpPage},
-
+  {path: '/provider', component: ProviderLogged},
   {
     path: '/profile',
     name: 'UserProfile',
