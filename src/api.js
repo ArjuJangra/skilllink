734040d3058159104b5ca26b92648828c1ec254f
Parveendhillon <parveendhillo08@gmail.com>
2025-08-21 23:47:55 +0530

    commit

--- src/api.js
+++ src/api.js
@@ -2,9 +2,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000', 
+  baseURL: process.env.VUE_APP_API_URL || "http://localhost:5000", 
 });
-
 
 API.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
