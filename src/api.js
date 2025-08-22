e735628446620ba918f75bb84fe390b30a89f834
Parveendhillon <parveendhillo08@gmail.com>
2025-08-22 10:15:15 +0530

    commit

--- src/api.js
+++ src/api.js
@@ -1,15 +1,29 @@
 // src/api.js
 import axios from 'axios';
 
+let baseURL;
+
+// If running locally → use local API
+if (window.location.hostname === 'localhost') {
+  baseURL = process.env.VUE_APP_API_URL;
+} else {
+  // Else → use prod API from environment
+  baseURL = process.env.VUE_APP_API_URL_PROD;
+}
+
 const API = axios.create({
-  baseURL: process.env.VUE_APP_API_URL + 'api', 
+  baseURL: baseURL + 'api',
 });
 
 API.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
   return config;
 });
 
 export default API;
+
+
 
