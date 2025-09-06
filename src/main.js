3369df502fa638744738a3f0025b8402d3bbb6bd
Parveendhillon <parveendhillo08@gmail.com>
2025-09-06 15:10:19 +0530

    commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import './assets/main.css';
 import './assets/tailwind.css';
 import Vue3Toastify from 'vue3-toastify';
 import 'vue3-toastify/dist/index.css';
